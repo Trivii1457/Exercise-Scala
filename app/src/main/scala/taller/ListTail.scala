de2bc0aaf1387530d3cc4f0a7c2f0988d83c2121
Trivii1457 <trivi1457@gmail.com>
2024-09-12 10:59:56 -0500

    Agrego test punto 1

--- app/src/main/scala/taller/ListTail.scala
+++ app/src/main/scala/taller/ListTail.scala
@@ -4,17 +4,23 @@ import scala.annotation.tailrec
 
 class ListTail {
 
+  //funcion que retorna el maximo de una lista
+  //ingresa una lista de enteros y retorna el maximo de la lista
   def maxLin(l: List[Int]): Int = {
 
+    //funcion auxiliar que retorna el maximo de una lista
     @tailrec
     def maxLinAux(l: List[Int], max: Int): Int = {
-      if (l.isEmpty) max
+      if (l.isEmpty) max//si la lista esta vacia retorna el maximo
+      //si no esta vacia se llama a si misma con la cola de la lista
+      // y el maximo entre el primer elemento de la lista y el maximo
       else maxLinAux(l.tail, if (l.head > max) l.head else max)
     }
-
+    //llamado a la funcion auxiliar
     maxLinAux(l, l.head)
   }
 
+  
   def maxIt(l: List[Int]): Int = {
 
     @tailrec
--- app/src/test/scala/taller/ListTailTest.scala
+++ app/src/test/scala/taller/ListTailTest.scala
@@ -3,18 +3,21 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ListTailTest extends AnyFunSuite {
 
-  test("maxLin should return the maximum element in the list") {
+  test("maxLin debería retornar el elemento máximo en la lista") {
     val listTail = new ListTail
     assert(listTail.maxLin(List(1, 2, 3, 4, 5)) == 5)
     assert(listTail.maxLin(List(5, 4, 3, 2, 1)) == 5)
     assert(listTail.maxLin(List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)) == 9)
   }
 
-  test("maxIt should return the maximum element in the list") {
+  test("maxLin debería lanzar una excepción para una lista vacía") {
     val listTail = new ListTail
-    assert(listTail.maxIt(List(1, 2, 3, 4, 5)) == 5)
-    assert(listTail.maxIt(List(5, 4, 3, 2, 1)) == 5)
-    assert(listTail.maxIt(List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)) == 9)
+    assertThrows[NoSuchElementException] {
+      listTail.maxLin(List())
+    }
   }
+
+  
+
 
 }
